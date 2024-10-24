6266f887a2a0b127adf2757407dba0c810606482
Nicolas Roger <nramonteirods05@gmail.com>
2024-10-24 19:25:57 +0000

    final_commit

--- aula_inicial.py
+++ aula_inicial.py
@@ -6,3 +6,9 @@ print("Adicionando funcionalidade: função inss.")
 def inss():
     # Considere feito.
     pass
+
+# git branch ## para verificar quantas branches existem
+# git branch -b e nome da branch ## para criar uma nova branch
+# git checkout e nome da branch ## para mudar de branch
+# git branch -d e nome da branch ## para deletar uma branch ## é necessário estar na main branch para deletar outras branches
+# git merge e nome da branch para juntar alterações de uma branch em outra. 
